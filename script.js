8ede8f20960c688aedb236d76b9dc6d8a4415da9
zoyanaaznasar-sketch <zoyanaaz.nasar@gmail.com>
2026-01-30 18:17:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('.nav-right a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
